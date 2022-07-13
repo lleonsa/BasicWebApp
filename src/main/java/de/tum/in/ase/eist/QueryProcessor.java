a5e437066f9e8a4228ef39a030267b1afd3c57ee
ge78nim <Mvmu@MHJ9.HsXX6>
2022-07-13 11:35:38 +0200

    test commit 4

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "Leon";
+        } else if (query.contains("name")) {
+            return "Leon";
         } else { // TODO extend the programm here
             return "";
         }
